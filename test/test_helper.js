846ae4ace9bc4b26b60e2947e75da4a803abcde2
Maziar <Maziar.mod@gmail.com>
2023-02-22 10:01:05 -0500

    add before() call in test_helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
+//mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/users_test');
-mongoose.connection
-  .once('open', () => {
-    console.log('Good to go!');
-  })
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  });
+before((done) => {
+  mongoose.connect('mongodb://127.0.0.1:27017/users_test');
+  mongoose.connection
+    .once('open', () => {
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
