5f2f66afa0d7c34cc1ac72146ba9b15406b1f61f
Maziar <Maziar.mod@gmail.com>
2023-02-22 17:29:43 -0500

    add validation_test

--- src/user.js
+++ src/user.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 // create schema
 const UserSchema = new Schema({
-  name: String,
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.',
+    },
+    required: [true, 'Name is required.'],
+  },
   postCount: Number,
 });
 
